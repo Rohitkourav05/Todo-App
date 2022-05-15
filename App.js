52db7b0ddac11283b4e5eff4da6a2f7014b360fa
Rohit kourav <89316651+Rohitkourav05@users.noreply.github.com>
2022-05-16 03:43:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Todo from './Components/Todo';
+import { arr } from './Components/Todo';
+
+function App() {
+  console.log(arr)
+  return (
+    <Todo />
+  );
+}
+
+export default App;
